73fe0eccf459d16f55ef737830b3db281f9ad7d6
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-10-30 15:26:23 -0400

    Frontend complete.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+
 function Projects(props) {
-    return <h1>Projects</h1>;
+    //  Create state to hold projects
+    const [projects, setProjects] = useState(null);
+
+    //  Create a function to make api call
+    const getProjectsData = async () => {
+        //  Make api call and get response 
+        const response = await fetch(props.URL + "projects");
+        //  Turn the response into a javascript object
+        const data = await response.json();
+        //  Set the projects state to the data
+        setProjects(data);
+    };
+
+    //  Make an initial call for the data inside a useEffect, so it only happens once a component loads
+    useEffect(() => getProjectsData(), [])
+
+    //  Define a function that will return the JSX needed once we get the data
+    const loaded = () => {
+        return projects.map((project) => (
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} />
+                <a href={project.git}>
+                    <button>Github</button>
+                </a>
+                <a href={project.live}>
+                    <button>live site</button>
+                </a>
+            </div>
+        ));
+    };
+    return projects ? loaded() : <h1>Loading...</h1>;
 }
 
 export default Projects;
